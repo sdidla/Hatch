be9e830b3e4014cfe5015ac94975051a3999fb92
Shammi Didla <shammi.didla@xing.com>
2022-04-02 19:29:59 +0200

    try gen-doc.sh

--- gen-doc.sh
+++ gen-doc.sh
@@ -5,8 +5,10 @@ echo "Hello docs"
 swift package \
     --allow-writing-to-directory "docs" \
     generate-documentation \
-    --target Hatch \
+    --target HatchParser \
     --disable-indexing \
     --transform-for-static-hosting \
     --hosting-base-path docs \
     --output-path "docs"
+
+ls -laR
