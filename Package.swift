4aac5aeaab681ec6026db188b1cb7696398fc454
Shammi Didla <sdidla@gmail.com>
2024-12-04 10:55:10 +0100

    support swift 5.x and 6.x

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -30,5 +30,6 @@ let package = Package(
             ]
         ),
         .testTarget(name: "HatchTests", dependencies: ["Hatch"])
-    ]
+    ],
+    swiftLanguageVersions: [ .version("6"), .v5]
 )
